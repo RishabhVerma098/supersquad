1523116ab2a7624700591fe2124eace7680e3b32
rishabh verma <vermarishabh0987@gmail.com>
2019-03-19 22:08:37 +0530

    made first reducers

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+
+import App from "./components/App";
+import rootReducers from "./reducers";
+
+const store = createStore(rootReducers);
+// seeing what is in the state for now
+console.log(store.getState());
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+import charactor_json from "../data/characters";
+
+function characters(state = charactor_json, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
+
+export default characters;
