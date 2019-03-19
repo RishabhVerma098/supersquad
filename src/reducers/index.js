0f41e16501f60487ba8bcb4414e9afb88b3e6fb1
rishabh verma <vermarishabh0987@gmail.com>
2019-03-19 22:41:15 +0530

    added cesond reducers yo add ch into new array (component 2)

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,8 @@
 import charactor_json from "../data/characters";
 import { ADD_CHARACTER } from "../actions";
+import { combineReducers } from "redux";
 
-function characters(state = charactor_json, action) {
+const characters = (state = charactor_json, action) => {
   switch (action.type) {
     case ADD_CHARACTER:
       let characters = state.filter(item => item.id !== action.payload);
@@ -9,6 +10,26 @@ function characters(state = charactor_json, action) {
     default:
       return state;
   }
-}
+};
 
-export default characters;
+const heros = (state = [], action) => {
+  switch (action.type) {
+    case ADD_CHARACTER:
+      let hero = [...state, heros_helper(action.payload)];
+      return hero;
+    default:
+      return state;
+  }
+};
+
+const heros_helper = id => {
+  let characters = charactor_json.find(c => c.id === id);
+  return characters;
+};
+
+const rootReducers = combineReducers({
+  characters,
+  heros
+});
+
+export default rootReducers;
